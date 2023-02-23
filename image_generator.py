93dc2e62d84033026b794e6150e7cb6d845fa3f5
Paolo Reyes <paolo.alfonso.reyes@gmail.com>
2023-02-22 22:40:22 -0600

    Sumador de 8 Bits

--- image_generator.py
+++ image_generator.py
@@ -1,23 +1,32 @@
 from manim import *
 
-class ImageGenerator(Scene):
+class TTGenerator(Scene):
     def construct(self):
         self.camera.background_color = "#353537"
-        c1 = WHITE
-        c2 = WHITE
-        c3 = WHITE
         t4 = IntegerTable(
-            [(0, 0, 0),
-             (0, 1, 0),
-             (1, 0, 0),
-             (1, 1, 1)],
-            col_labels=[Tex("A", color=c1), Tex("B", color=c2), Tex("C", color=c3)],
+            [(0, 0, 0, 0, 0),
+             (0, 0, 1, 0, 1),
+             (0, 1, 0, 0, 1),
+             (0, 1, 1, 1, 0),
+             (1, 0, 0, 0, 1),
+             (1, 0, 1, 1, 0),
+             (1, 1, 0, 1, 0),
+             (1, 1, 1, 1, 1)],
+            col_labels=[MathTex("C_{in}"), Tex("X"), Tex("Y"), MathTex("C_{out}"), Tex("S")],
             include_outer_lines=True, 
-        )
-        t4.get_columns()[0].set_color(c1)
-        t4.get_columns()[1].set_color(c2)
-        t4.get_columns()[2].set_color(c3)
+        ).scale(0.7)
         tcolor = GRAY_C
         t4.get_horizontal_lines().set_color(tcolor)
         t4.get_vertical_lines().set_color(tcolor)
         self.add(t4)
+
+class LatexGenerator(Scene):
+    def construct(self):
+        self.camera.background_color = "#353537"
+        eq1 = MathTex(r"C_{i+1} = G_i+P_iC_{i}", color=BLUE).to_corner(UR)
+        eq2 = MathTex(r"C_{i+2} = G_{i+1}+P_{i+1}C_{i+1}")
+        eq3 = MathTex(r"C_{i+2} = G_{i+1}+P_{i+1}(G_{i}+P_{i}C_{i})")
+        eq2[0][14:].set_color(BLUE)
+        eq3[0][14:].set_color(BLUE)
+        eqs = VGroup(eq2, eq3).arrange_submobjects(DOWN, buff=0.6)
+        self.add(eqs, eq1)
