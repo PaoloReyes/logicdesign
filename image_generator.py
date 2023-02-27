8f85723edd810bef424f5ddeeeb604a42297aacf
Paolo Reyes <paolo.alfonso.reyes@gmail.com>
2023-02-26 23:06:31 -0600

    Detector de primos (En proceso)

--- image_generator.py
+++ image_generator.py
@@ -5,16 +5,24 @@ class TTGenerator(Scene):
         self.camera.background_color = "#353537"
         t4 = IntegerTable(
             [(0, 0, 0, 0, 0),
+             (0, 0, 0, 1, 0),
              (0, 0, 1, 0, 1),
-             (0, 1, 0, 0, 1),
-             (0, 1, 1, 1, 0),
-             (1, 0, 0, 0, 1),
-             (1, 0, 1, 1, 0),
-             (1, 1, 0, 1, 0),
-             (1, 1, 1, 1, 1)],
-            col_labels=[MathTex("C_{in}"), Tex("X"), Tex("Y"), MathTex("C_{out}"), Tex("S")],
+             (0, 0, 1, 1, 1),
+             (0, 1, 0, 0, 0),
+             (0, 1, 0, 1, 1),
+             (0, 1, 1, 0, 0),
+             (0, 1, 1, 1, 1),
+             (1, 0, 0, 0, 0),
+             (1, 0, 0, 1, 0),
+             (1, 0, 1, 0, 0),
+             (1, 0, 1, 1, 1),
+             (1, 1, 0, 0, 0),
+             (1, 1, 0, 1, 1),
+             (1, 1, 1, 0, 0),
+             (1, 1, 1, 1, 0)],
+            col_labels=[MathTex("X_3"), MathTex("X_2"), MathTex("X_1"), MathTex("X_0"), Tex("P")],
             include_outer_lines=True, 
-        ).scale(0.7)
+        ).scale(0.4)
         tcolor = GRAY_C
         t4.get_horizontal_lines().set_color(tcolor)
         t4.get_vertical_lines().set_color(tcolor)
@@ -23,10 +31,5 @@ class TTGenerator(Scene):
 class LatexGenerator(Scene):
     def construct(self):
         self.camera.background_color = "#353537"
-        eq1 = MathTex(r"C_{i+1} = G_i+P_iC_{i}", color=BLUE).to_corner(UR)
-        eq2 = MathTex(r"C_{i+2} = G_{i+1}+P_{i+1}C_{i+1}")
-        eq3 = MathTex(r"C_{i+2} = G_{i+1}+P_{i+1}(G_{i}+P_{i}C_{i})")
-        eq2[0][14:].set_color(BLUE)
-        eq3[0][14:].set_color(BLUE)
-        eqs = VGroup(eq2, eq3).arrange_submobjects(DOWN, buff=0.6)
-        self.add(eqs, eq1)
+        eq1 = MathTex(r"\left(\overline{X_3}+\overline{X_2}+\overline{X_1}\right)+\left(\overline{X_3}+X_0\right)+\left(\overline{X_2}+X_0\right)+\left(X_2+X_1\right)")
+        self.add(eq1)
